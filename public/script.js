911ea558948b1c92618f044d1600c3ee4417c435
Marcel Muller <com2040@gmail.com>
2013-06-14 18:37:48 +0200

    Fixed Analytics

--- public/script.js
+++ public/script.js
@@ -23,7 +23,7 @@ $(function(){
       if(!force){
         history.pushState({loadable: true},null,path)
       }
-      _gaq.push(['_trackPageview', path]);
+      ga('send', 'pageview');
       pathChanged()
       
     }).error(function(){
@@ -63,20 +63,17 @@ $(function(){
 
   })
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-41721936-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  function google(i,s,o,g,r,a,m){
+    i['GoogleAnalyticsObject']=r;
+    i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)
+    }, i[r].l=1*new Date();
+    a=s.createElement(o),  m=s.getElementsByTagName(o)[0];
+    a.async=1;
+    a.src=g;
+    m.parentNode.insertBefore(a,m)
+  }
+  google(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-41721936-1', 'roleplayinginequestria.com');
   ga('send', 'pageview');
